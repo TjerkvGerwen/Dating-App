633e53648f8db0123e1973fb85fdabf2d2c2a45f
TjerkvGerwen <tjerkvangerwen@gmail.com>
2020-05-18 14:24:42 +0200

    vork

--- Dating_App.py
+++ Dating_App.py
@@ -2,25 +2,35 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os
 import pyodbc
+counter = 1
+
 root = tk.Tk()
 root.title("DatingAAP")
 root.geometry("600x400")
-#userdb = 'datingapp.dbo.persgegevens(voornaam, lidnummer)'
-#questdb = 'datingapp.dbo.quizvragen()'
-#chosendb = 'nog niks'
 conn = pyodbc.connect('Driver={SQL Server};'
                           'Server=DATINGAPP;'
                           'Database=datingapp;'
                           'UID=computerland;''PWD=P@ssw0rd')
-
 cursor = conn.cursor()
+
+cursor.execute("SELECT COUNT (*) FROM persgegevens")
+lidnummercounter = cursor.fetchone()[0] + 1
+
+print (lidnummercounter)
+
 def getTextInput():
     result=textExample.get(1.0, tk.END+"-1c")
 
     cursor.execute("INSERT INTO persgegevens (voornaam, lidnummer) VALUES ("+result+")")
     conn.commit()
 
+def buttonA():
+    cursor.execute("INSERT INTO quizvragen (geslacht) VALUES(A)")
+    conn.commit()
 
+def buttonB():
+    cursor.execute("INSERT INTO persgegevens (voornaam, lidnummer) VALUES (" + result + ")")
+    conn.commit()
 
 textExample=tk.Text(root, height= 1)
 textExample.pack()
@@ -30,19 +40,13 @@ btnRead.pack()
 AntwoordA=tk.Button(root, height=2, width=20, text="A").place(x=10, y=350)
 
 
-AntwoordB=tk.Button(root, height=2, width=20, text="B").place(x=225, y=350)
-
-
-AntwoordC=tk.Button(root, height=2, width=20, text="C").place(x=440, y=350)
+AntwoordB=tk.Button(root, height=2, width=20, text="B").place(x=440, y=350)
 
 
 root.mainloop()
 
 
-def insertdb(chosendb):
-    cursor.execute('INSERT INTO'+ chosendb + 'VALUES' () )
 
-    return;
 
 
 
